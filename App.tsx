6c78ab008cf91a4ade882c6810c251cc3623598d
zhangbowen <1290443925@qq.com>
2025-07-26 14:15:49 +0800

    2025 7-26 14:16 使用cli命令创建ReactNative工程，并启动运行

--- App.tsx
+++ App.tsx
@@ -6,13 +6,20 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  Text,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
+      <Text style={styles.text}>Hello ZBW</Text>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NewAppScreen templateFileName="App.tsx" />
     </View>
@@ -22,6 +29,12 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  text: {
+    textAlign: 'center',
+    color: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
